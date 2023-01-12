977d8aacb34dc515863e812afe84c649866c56d6
_Roni W_ <_[roni.witbeck@gmail.com](mailto:roni.witbeck@gmail.com)_>
2023-01-12 12:40:13 -0800

    project complete

--- app.js
+++ app.js
@@ -4,7 +4,54 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Roni";
+let lastName = "Witbeck";
+var age = 31;
 
 // Exercise 2
+let fullName = `${firstName} ${lastName}`;
+
+console.log(
+  `My name is ${fullName}.`
+)
 
 // Exercise 3
+let city = "Seattle";
+let pastime = "dance and rock climbing";
+
+console.log(
+  `Hello! My name is ${fullName}. I live in ${city} and enjoy ${pastime}.`
+)
+
+// Exercise 4
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log(
+  `${language} was first released in ${createdYear}.`
+);
+
+// Exercise 5
+let product = "Sweatshirt";
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+console.log(
+  `${product} in size ${selectedSize} is ${price} with ${stock} in stock.`
+);
+
+// Exercise 6
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log(
+  `${title} by ${author} is ${pageCount} pages long. Your last bookmark was page ${bookmark}.`
+);
